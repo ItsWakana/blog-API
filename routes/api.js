04dda86aa50d5c02390a0fa9d574321cd72a1f0a
wakana <wakanawardrobe@gmail.com>
2023-09-19 16:58:27 +0100

    continue working on endpoints for api

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 
-router.post("/users", (req, res) => {
+router.post("/users", async (req, res) => {
     const {
         name,
         username,
@@ -17,11 +18,40 @@ router.post("/users", (req, res) => {
         isAdmin
     });
 
-    //add this user to the database
-
-    newUser.save();
-
-    res.json(newUser);
+    try {
+        await newUser.save();
+    
+        res.json(newUser);
+    } catch(err) {
+        res.json({errorMessage: err});
+    }
 });
+
+router.get("/users", async (req, res) => {
+
+    try {
+
+        const foundUsers = await User.find();
+        if (!foundUsers) {
+            res.sendStatus(404).json({message: "No users found"});
+        } else {
+            res.json(foundUsers);
+        }
+    } catch(err) {
+        res.sendStatus(404).json({errorMessage: err});
+    }
+});
+
+// GETTING ALL POSTS
+
+router.get("/posts", async (req, res) => {
+
+    try {
+        const foundPosts = await Post.find();
+
+    } catch(err) {
+        res.sendStatus(404).json({ errorMessage: err });
+    }
+})
 
 module.exports = router;
